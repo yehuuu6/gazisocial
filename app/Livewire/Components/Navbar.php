f6bcb0db8e5c48fbcddab83d7e9f99abbea64129
yehuuu6 <therenaydin@gmail.com>
2024-08-27 14:05:04 +0300

    Add destroy method to logout

--- app/Livewire/Components/Navbar.php
+++ app/Livewire/Components/Navbar.php
@@ -3,9 +3,16 @@
 namespace App\Livewire\Components;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Navbar extends Component
 {
+
+    public function destroy(){
+        Auth::logout();
+        return $this->redirect('/login', navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.components.navbar');
