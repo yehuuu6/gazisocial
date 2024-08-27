9e899282d7211244456451b75c4e234941cbb157
yehuuu6 <therenaydin@gmail.com>
2024-08-28 02:06:05 +0300

    Dispatch event to scroll to top

--- app/Livewire/Pages/Posts/ShowPost.php
+++ app/Livewire/Pages/Posts/ShowPost.php
@@ -5,14 +5,19 @@ namespace App\Livewire\Pages\Posts;
 use Livewire\Component;
 use App\Models\Post;
 use Livewire\WithPagination;
+use Livewire\WithoutUrlPagination;
 
 class ShowPost extends Component
 {
 
-    use WithPagination;
+    use WithPagination, WithoutUrlPagination;
 
     public Post $post;
 
+    public function updatingPage($page)
+    {
+        $this->dispatch('scroll-to-header');
+    }
     public function paginationSimpleView()
     {
         return 'livewire.pagination.simple';
