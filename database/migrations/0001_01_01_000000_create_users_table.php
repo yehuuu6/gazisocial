281d183f70800a8fcfdb9e1a1c9a0b3d5e42ec65
yehuuu6 <therenaydin@gmail.com>
2024-08-27 13:09:14 +0300

    Setup migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('avatar')->nullable();
+            $table->boolean('is_gazi')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
