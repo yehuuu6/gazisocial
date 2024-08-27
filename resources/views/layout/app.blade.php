cfc2f2b1d8644396a1a38410a2a142417385399b
yehuuu6 <therenaydin@gmail.com>
2024-08-27 16:07:47 +0300

    Update layout structure

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,10 +9,36 @@
     <title>{{ $title ?? 'Gazi Social' }}</title>
 </head>
 
-<body class="antialiased h-screen flex flex-col bg-gray-100">
+@php
+    $categories = [
+        (object) ['name' => 'Genel', 'slug' => 'genel'],
+        (object) ['name' => 'Teknoloji', 'slug' => 'teknoloji'],
+        (object) ['name' => 'Duyurular', 'slug' => 'duyurular'],
+        (object) ['name' => 'Etkinlik', 'slug' => 'etkinlik'],
+    ];
+@endphp
+
+<body class="antialiased h-screen flex flex-col bg-gray-50 overflow-hidden">
     <livewire:components.navbar />
     <main class="flex-1">
-        {{ $slot }}
+        <div class="h-full grid grid-cols-[320px_1fr] gap-6 p-6">
+            <div class="bg-white shadow-md rounded-xl flex flex-col overflow-hidden">
+                <h3 class="text-xl font-bold p-4">Kategoriler</h3>
+                <x-seperator />
+                <ul class="flex flex-col gap-1 p-4">
+                    @foreach ($categories as $category)
+                        <li>
+                            <a class="flex items-center text-lg gap-2 hover:bg-[#E5E7EB]/50 px-2 py-1 rounded-md font-normal"
+                                href="/categories/{{ $category->slug }}"
+                                wire:navigate><x-icons.folder />{{ $category->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+                <x-seperator />
+                <x-users.user-activities />
+            </div>
+            {{ $slot }}
+        </div>
     </main>
     <footer class="bg-gray-800 text-white p-4 font-medium flex justify-between items-center">
         <h4>Copyright all rights reserved.</h4>
