ce7d7d63b5b1827f013fda8c7817bace3d18ace6
yehuuu6 <therenaydin@gmail.com>
2024-08-27 16:07:17 +0300

    Display latest posts

--- resources/views/livewire/pages/home-page.blade.php
+++ resources/views/livewire/pages/home-page.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <h1>Hello world</h1>
+<div class="bg-white shadow-md rounded-xl flex flex-col overflow-hidden">
+    <x-header-title>
+        En Son Konular
+    </x-header-title>
+
+    <x-seperator />
+    <div class="relative h-full">
+        <ul wire:loading class="overflow-y-hidden absolute h-full w-full">
+            @for ($i = 0; $i < 10; $i++)
+                <x-posts.placeholder />
+            @endfor
+        </ul>
+        <ul wire:loading.remove class="overflow-y-auto absolute h-full w-full">
+            @foreach ($posts as $post)
+                <x-posts.item :$post />
+            @endforeach
+        </ul>
+    </div>
+    {{ $posts->links() }}
 </div>
