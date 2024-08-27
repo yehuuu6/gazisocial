2a836052740f56a34852a858480eb5e4163a8cf9
yehuuu6 <therenaydin@gmail.com>
2024-08-27 14:03:40 +0300

    Add livewire pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Pages\HomePage;
+use App\Livewire\Pages\Auth\Login;
+use App\Livewire\Pages\Auth\Register;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomePage::class)->name('home');
+
+// Auth routes
+Route::middleware('guest')->group(function () {
+    Route::get('/register', Register::class)->name('register');
+    Route::get('/login', Login::class)->name('login');
 });
