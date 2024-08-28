6b0b39697262d2025707dac7759868d16b6cc0f4
yehuuu6 <therenaydin@gmail.com>
2024-08-28 13:56:32 +0300

    Add title

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Giriş Yap - Gazi Social</title>
+    <title>{{ $title }} - {{ config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
