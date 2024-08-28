77a7941d82d42bcb3827fed5bc1df31f1b7ae927
yehuuu6 <therenaydin@gmail.com>
2024-08-28 21:55:28 +0300

    Add custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ export default {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
         extend: {
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+                showUp: 'showUp 0.5s ease-in-out',
+                fadeIn: 'fadeIn 0.5s ease-in-out',
+            },
+            keyframes: {
+                wiggle: {
+                  '0%, 100%': { transform: 'rotate(-3deg)' },
+                  '50%': { transform: 'rotate(3deg)' },
+                },
+                showUp: {
+                    '0%': { transform: 'translateY(100%)', opacity: 0 },
+                    '100%': { transform: 'translateY(0)', opacity: 1 },
+                },
+                fadeIn: {
+                    '0%': { opacity: 0 },
+                    '100%': { opacity: 1 },
+                },
+            },
             colors: {
                 primary: "#0b3e75",
             },
