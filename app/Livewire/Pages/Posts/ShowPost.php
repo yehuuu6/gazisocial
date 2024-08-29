7f06a8e86d13dc37ca6ece62fd40292051baf849
yehuuu6 <therenaydin@gmail.com>
2024-08-29 22:48:39 +0300

    Add event listener for create comment method

--- app/Livewire/Pages/Posts/ShowPost.php
+++ app/Livewire/Pages/Posts/ShowPost.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use App\Models\Post;
 use Livewire\WithPagination;
 use Livewire\WithoutUrlPagination;
+use Livewire\Attributes\On;
 
 class ShowPost extends Component
 {
@@ -21,6 +22,12 @@ class ShowPost extends Component
     public function paginationSimpleView()
     {
         return 'livewire.pagination.simple';
+    }
+
+    #[On('comment-created')]
+    public function refreshPage(){
+        $this->resetPage();
+        $this->dispatch('scroll-to-header');
     }
 
     public function render()
