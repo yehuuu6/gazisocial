008990781c87b0346627d2ca09439b50d5256761
yehuuu6 <therenaydin@gmail.com>
2024-08-29 22:43:04 +0300

    Install wire-elements/modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    content: [
+        './vendor/wire-elements/modal/resources/views/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js"
+    ],
+    safelist: [
+        {
+          pattern: /max-w-(sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl)/,
+          variants: ['sm', 'md', 'lg', 'xl', '2xl']
+        }
+      ],
     theme: {
         extend: {
             animation: {
