4cd4dfc5035087a58858f8aecbfc74df85d7a077
yehuuu6 <therenaydin@gmail.com>
2024-08-29 22:43:25 +0300

    Remove logout method

--- app/Livewire/Components/Navbar.php
+++ app/Livewire/Components/Navbar.php
@@ -3,16 +3,9 @@
 namespace App\Livewire\Components;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Auth;
 
 class Navbar extends Component
 {
-
-    public function destroy(){
-        Auth::logout();
-        return $this->redirect('/login', navigate: true);
-    }
-
     public function render()
     {
         return view('livewire.components.navbar');
