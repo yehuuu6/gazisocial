9994b4729e63fb3f110d1ebfb592f774db3558dc
yehuuu6 <therenaydin@gmail.com>
2024-08-30 14:45:41 +0300

    Remove some public properties

--- app/Livewire/Components/Post/Comment.php
+++ app/Livewire/Components/Post/Comment.php
@@ -8,14 +8,9 @@ class Comment extends Component
 {
 
     public $comment;
-    public $username;
-    public $time;
 
     public function render()
     {
-        $this->username = "@{$this->comment->user->username}";
-        $this->time = $this->comment->created_at->diffForHumans();
-
         return view('livewire.components.post.comment');
     }
 }
--- app/Livewire/Components/Post/Details.php
+++ app/Livewire/Components/Post/Details.php
@@ -10,14 +10,8 @@ class Details extends Component
 
     public Post $post;
 
-    public string $username;
-
-    public string $time;
-
     public function render()
     {
-        $this->username = "@{$this->post->user->username}";
-        $this->time = $this->post->created_at->diffForHumans();
         return view('livewire.components.post.details');
     }
 }
