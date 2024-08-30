b18c80997f5042927127493972fb61173f61320e
yehuuu6 <therenaydin@gmail.com>
2024-08-30 14:46:27 +0300

    Add bio and created_at for factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,9 +29,11 @@ class UserFactory extends Factory
             'username' => fake()->unique()->userName(),
             'avatar' => 'https://ui-avatars.com/api/?name=' . urlencode($name) . '&background=random',
             'email' => fake()->unique()->safeEmail(),
+            'bio' => fake()->sentence(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
         ];
     }
 
