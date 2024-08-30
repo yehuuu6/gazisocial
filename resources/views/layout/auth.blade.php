59981070a0330453e4af3f7b893c6230fd0526c6
yehuuu6 <therenaydin@gmail.com>
2024-08-30 14:47:13 +0300

    Add livewire-alert scripts to layout

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-livewire-alert::scripts />
     <title>{{ $title }} - {{ config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-white">
+<body class="bg-white"">
     {{ $slot }}
 </body>
+@livewire('wire-elements-modal')
 
 </html>
