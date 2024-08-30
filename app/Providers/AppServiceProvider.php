eaaec3ac7daac29422913b319d12958fd176abea
yehuuu6 <therenaydin@gmail.com>
2024-08-30 14:46:17 +0300

    Set Carbon language to Turkish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::preventLazyLoading();
 
-        JsonResource::withoutWrapping();
+        Carbon::setLocale('tr');
+
+        Model::preventLazyLoading();
 
     }
 }
