d281469f0e008f3d0d509fe5a49a87331da5919b
yehuuu6 <therenaydin@gmail.com>
2024-08-30 14:44:48 +0300

    Add bio

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
+            $table->text('bio')->nullable();
             $table->string('avatar')->nullable();
             $table->boolean('is_gazi')->default(false);
             $table->string('email')->unique();
