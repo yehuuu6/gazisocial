77e41c1be42c219e29e75e15e995fd0418b8e460
yehuuu6 <therenaydin@gmail.com>
2024-08-31 14:18:05 +0300

    Create roles and pivot tables and update others.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->text('bio')->nullable();
             $table->string('avatar')->nullable();
-            $table->boolean('is_gazi')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
