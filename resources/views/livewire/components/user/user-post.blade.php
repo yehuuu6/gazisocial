0e3b1901e2d1fcf2a4b9448b76a7966807dcfd7a
yehuuu6 <therenaydin@gmail.com>
2024-08-31 21:03:48 +0300

    Create new component for user posts

---
+++ resources/views/livewire/components/user/user-post.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col p-4">
+    <div class="flex justify-between items-center">
+        <x-link href="/posts/{{ Str::slug($post->title) }}"
+            class="text-lg font-medium text-blue-500">{{ $post->title }}</x-link>
+        <span class="text-gray-500">{{ $post->created_at->diffForHumans() }}</span>
+    </div>
+    <p class="mt-2 text-gray-500">{{ Str::limit($post->content, 150, '...') }}</p>
+</div>
