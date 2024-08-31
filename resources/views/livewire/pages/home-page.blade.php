f6219d1ace710f37d3734b4a9f8c95c9e22f2fce
yehuuu6 <therenaydin@gmail.com>
2024-08-31 15:36:52 +0300

    Fix not scrolling to top on pagination

--- resources/views/livewire/pages/home-page.blade.php
+++ resources/views/livewire/pages/home-page.blade.php
@@ -5,32 +5,28 @@
     </x-header-title>
 
     <x-seperator />
-    <x-scrollable-wrapper class="h-full">
+    <x-scrollable-wrapper class="h-full" id="post-index">
         <ul wire:loading class="divide-y flex flex-1 flex-col gap-1 pb-5 w-full">
             @for ($i = 0; $i < 10; $i++)
                 <x-posts.placeholder />
             @endfor
         </ul>
-        <ul wire:loading.remove id="post-index" class="divide-y flex flex-1 flex-col gap-1 pb-5">
+        <ul wire:loading.remove class="divide-y flex flex-1 flex-col gap-1 pb-5">
             @foreach ($posts as $post)
                 <livewire:components.post.post-item :$post :key="$post->id" />
             @endforeach
         </ul>
     </x-scrollable-wrapper>
-    {{ $posts->links() }}
+    {{ $posts->links('livewire.pagination.simple') }}
 </div>
 @script
     <script>
-        // Not working after converting the post-item component into livewire component.
-        // Console log is visible but the scroll is not working.
-        document.addEventListener('livewire:initialized', function() {
-            Livewire.on('scroll-to-top', function() {
-                console.log('scroll-to-top');
-                const postIndexer = document.getElementById('post-index');
-                postIndexer.scroll({
-                    top: 0,
-                    behavior: 'smooth'
-                });
+        $wire.on('scroll-to-top', function() {
+            console.log('scroll-to-top');
+            const postIndexer = document.getElementById('post-index');
+            postIndexer.scroll({
+                top: 0,
+                behavior: 'smooth'
             });
         });
     </script>
