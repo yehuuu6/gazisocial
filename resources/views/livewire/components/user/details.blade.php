585936c01de70b3b3545c20bf929bee6e8ac2e16
yehuuu6 <therenaydin@gmail.com>
2024-08-31 21:05:31 +0300

    Update page structure

--- resources/views/livewire/components/user/details.blade.php
+++ resources/views/livewire/components/user/details.blade.php
@@ -12,38 +12,42 @@
     <div class="flex flex-col gap-3 flex-1">
         <div class="flex items-center justify-between">
             <div class="flex gap-1 items-end">
-                <h1 class="text-xl font-bold">{{ $user->name }}</h1>
-                <div class="space-x-1">
-                    <span class="text-gray-500 text-sm">{{ '@' . $user->username }}</span>
+                <h1 class="text-xl font-medium">{{ $user->name }}</h1>
+                <div class="flex items-center gap-2">
+                    <span class="text-gray-600 text-sm">{{ '@' . $user->username }}</span>
                     @forelse ($user->roles as $role)
                         <span
-                            class="py-1 px-2 {{ $colorVariants[$role->color] }} text-white font-medium rounded-full capitalize text-xs">{{ $role->name }}</span>
+                            class="py-1 px-2 {{ $colorVariants[$role->color] }} text-white font-medium rounded capitalize text-xs">{{ $role->name }}</span>
                     @empty
                         <span
                             class="py-1 px-2 bg-gray-700 text-white font-medium rounded-full capitalize text-xs">Üye</span>
                     @endforelse
                 </div>
             </div>
-            <x-icons.settings />
+            <x-icons.settings color="#4b5563" size="30" />
         </div>
-        <p class="text-gray-500">{{ $bio }}</p>
+        <p class="text-gray-600">{{ $bio }}</p>
         <div class="flex gap-3 items-center">
             <div class="flex gap-1 items-center">
-                <x-icons.calender />
-                <span class="text-gray-500">{{ $user->created_at->translatedFormat('F Y') }}
-                    tarihinden beri</span>
+                <x-icons.graduate color="#4b5563" size="28" />
+                <span class="text-gray-600">{{ $user->faculty ?? 'Hemşirelik Fakültesi' }}</span>
             </div>
             <div class="flex items-center gap-1">
-                <x-icons.trophy /><span class="text-gray-500">Seviye 10</span>
+                <x-icons.trophy color="#4b5563" size="30" /><span class="text-gray-600">Seviye 10</span>
             </div>
 
             <div class="flex items-center gap-1">
-                <x-icons.post />
-                <span class="text-gray-500">{{ $user->posts->count() }} Gönderi</span>
+                <x-icons.post color="#4b5563" size="25" />
+                <span class="text-gray-600">{{ $user->posts->count() }} Gönderi</span>
             </div>
             <div class="flex items-center gap-1">
-                <x-icons.comment-count />
-                <span class="text-gray-500">{{ $user->comments->count() }} Yorum</span>
+                <x-icons.comment-count color="#4b5563" size="26" />
+                <span class="text-gray-600">{{ $user->comments->count() }} Yorum</span>
+            </div>
+            <div class="flex gap-1 items-center">
+                <x-icons.calender color="#4b5563" size="28" />
+                <span class="text-gray-600">{{ $user->created_at->translatedFormat('F Y') }}
+                    tarihinden beri</span>
             </div>
         </div>
     </div>
