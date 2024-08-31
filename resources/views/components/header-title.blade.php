776507b81139bdc3edc6100ccd44a15a67fbbc7d
yehuuu6 <therenaydin@gmail.com>
2024-08-31 21:02:40 +0300

    Remove new post button from posts/create url

--- resources/views/components/header-title.blade.php
+++ resources/views/components/header-title.blade.php
@@ -2,7 +2,9 @@
     <div class="flex gap-2 items-center justify-start">
         <h3 class="text-xl font-bold py-4">{{ $slot }}</h3>
     </div>
-    <x-link href="/posts/create"
-        class="py-2 px-5 bg-blue-500 opacity-80 text-white rounded-md hover:opacity-100 focus:outline-none focus:ring-2 hover:no-underline">Konu
-        Aç</x-link>
+    @if (!request()->is('posts/create'))
+        <x-link href="/posts/create"
+            class="py-2 px-5 bg-blue-500 opacity-80 text-white rounded-md hover:opacity-100 focus:outline-none focus:ring-2 hover:no-underline">Konu
+            Aç</x-link>
+    @endif
 </div>
