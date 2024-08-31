554b4e0cc006672cdc6112d0a78272c6d6dfdb2c
yehuuu6 <therenaydin@gmail.com>
2024-08-31 21:06:12 +0300

    Create 'create post' view

---
+++ resources/views/livewire/pages/posts/create-post.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-white shadow-md rounded-xl flex flex-col overflow-hidden">
+    <x-header-title>
+        Konu Oluştur
+    </x-header-title>
+    <x-seperator />
+    <x-scrollable-wrapper class="h-full">
+        <form>
+            TODO
+        </form>
+    </x-scrollable-wrapper>
+</div>
