a9110bf2cff335f01c40362a47fddafe9639ece5
yehuuu6 <therenaydin@gmail.com>
2024-08-31 15:36:29 +0300

    Add optional div to make javascript scroll work

--- resources/views/components/scrollable-wrapper.blade.php
+++ resources/views/components/scrollable-wrapper.blade.php
@@ -1,5 +1,9 @@
+@props(['id' => ''])
+@php
+    $id = $id === '' ? '' : 'id=' . $id . '';
+@endphp
 <div {{ $attributes->merge(['class' => 'relative']) }}>
-    <div class="absolute overflow-y-auto overflow-x-hidden h-full w-full">
+    <div {{ $id }} class="absolute overflow-y-auto overflow-x-hidden h-full w-full">
         {{ $slot }}
     </div>
 </div>
