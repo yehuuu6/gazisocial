3fcb08b0af5b2333fe9602a5497bfc6501d0eaba
yehuuu6 <therenaydin@gmail.com>
2024-08-31 21:02:11 +0300

    Create policies

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CommentPolicy
+{
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        // If the user is not logged in, return false
+        if (!$user) {
+            return false;
+        }
+
+        // If the user is not verified, return false
+        if (!$user->hasVerifiedEmail()) {
+            return false;
+        }
+
+        return true;
+    }
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user->id;
+    }
+}
---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        // If the user is not logged in, return false
+        if (!$user) {
+            return false;
+        }
+
+        // If the user is not verified, return false
+        if (!$user->hasVerifiedEmail()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Post $post): bool
+    {
+        return $user->id === $post->user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Post $post): bool
+    {
+        return $user->id === $post->user->id;
+    }
+}
