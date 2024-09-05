b3d79db08c4000e2bf8e1159b75123eb66d5da3f
yehuuu6 <therenaydin@gmail.com>
2024-09-05 19:30:04 +0300

    Implement create post functionality

--- app/Livewire/Pages/Posts/CreatePost.php
+++ app/Livewire/Pages/Posts/CreatePost.php
@@ -3,9 +3,84 @@
 namespace App\Livewire\Pages\Posts;
 
 use Livewire\Component;
+use App\Models\Post;
+use App\Models\Poll;
+use App\Models\PollOption;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Illuminate\Support\Str;
+use Livewire\Attributes\On;
 
 class CreatePost extends Component
 {
+
+    use LivewireAlert;
+
+    public $title;
+    public $content;
+    public $createdPolls = [];
+
+    public function removePoll($index)
+    {
+        unset($this->createdPolls[$index]);
+        $this->createdPolls = array_values($this->createdPolls);
+    }
+
+    #[On('pollCreated')]
+    public function addPollToCache($poll)
+    {
+        // Set max poll count to 3
+        if (count($this->createdPolls) >= 3) {
+            $this->alert('error', 'En fazla 3 anket oluşturabilirsiniz.');
+            return;
+        }
+        // Insert the poll to the createdPolls array
+        $this->createdPolls[] = $poll;
+    }
+
+    public function createPost()
+    {
+        $response = Gate::inspect('create', Post::class);
+
+        if (!$response->allowed()) {
+            $this->alert('error', 'Konu oluşturmak e-posta onaylı bir hesap gerektirir.');
+            return;
+        }
+
+        $validated = $this->validate([
+            'title' => 'required|min:6',
+            'content' => 'required|min:10',
+        ]);
+
+        $slug = Str::slug($validated['title']);
+        $validated['slug'] = $slug;
+
+        // Create and save the post first
+        $post = Post::make($validated);
+        $post->user()->associate(Auth::user());
+        $post->save();
+
+        // Create poll options
+        if (count($this->createdPolls) > 0) {
+            foreach ($this->createdPolls as $poll) {
+                $pollModel = Poll::make(['question' => $poll['question']]);
+                $pollModel->post()->associate($post);
+                $pollModel->save();
+
+                foreach ($poll['options'] as $option) {
+                    $optionModel = PollOption::make(['option' => $option]);
+                    $optionModel->poll()->associate($pollModel);
+                    $optionModel->save();
+                }
+            }
+        }
+
+        session()->flash('post-created', 'Konu başarıyla oluşturuldu.');
+
+        return $this->redirect(route('post.show', $post->slug), navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.pages.posts.create-post')->title('Yeni Konu Oluştur - ' . config('app.name'));
--- resources/views/livewire/pages/posts/create-post.blade.php
+++ resources/views/livewire/pages/posts/create-post.blade.php
@@ -4,8 +4,60 @@
     </x-header-title>
     <x-seperator />
     <x-scrollable-wrapper class="h-full">
-        <form>
-            TODO
+        <form wire:submit="createPost">
+            <div class="p-6 space-y-4">
+                <div class="space-y-2">
+                    <label for="title" class="block text-sm font-medium text-gray-700">Başlık</label>
+                    <input wire:model="title" type="text" id="title" name="title"
+                        class="bg-gray-100 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                </div>
+                <div class="space-y-2">
+                    <label for="content" class="block text-sm font-medium text-gray-700">İçerik</label>
+                    <textarea wire:model="content" id="content" name="content" rows="5"
+                        class="bg-gray-100 resize-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                </div>
+                <h3 class="text-sm block font-medium text-gray-700">Anketler</h3>
+
+                <div class="flex gap-5">
+                    @foreach ($createdPolls as $poll)
+                        <div class="flex flex-col flex-grow gap-1 rounded-md border border-gray-300 bg-gray-100">
+                            <div class="flex items-center justify-between py-2 px-4">
+                                <h3 class="text-md text-gray-700 font-medium">{{ $poll['question'] }}</h3>
+                                <button wire:click.prevent="removePoll({{ $loop->index }})" type="button">
+                                    <x-icons.trash color='#ff6969' size='16' />
+                                </button>
+                            </div>
+                            <x-seperator />
+                            <div class="flex flex-col gap-2 py-1 pb-3">
+                                @foreach ($poll['options'] as $option)
+                                    <div class="flex items-center px-2 gap-1">
+                                        <span
+                                            class="px-3 py-2 bg-gray-200 rounded-full w-full text-gray-700">{{ $option }}</span>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                @if (count($createdPolls) === 0)
+                    <h3 class="text-gray-500">Hiç anket eklenmemiş.</h3>
+                @else
+                    <h3 class="text-orange-400">Oluşturduğunuz anketler taslaktır ve konuyu
+                        yayınlamadığınız
+                        sürece görünmeyecektir.</h3>
+                @endif
+                <div class="flex gap-2 justify-end">
+                    <button type="button"
+                        wire:click="$dispatch('openModal', { component: 'modals.create-poll-modal' })"
+                        class="py-2 px-5 border border-green-500 bg-transparent text-green-500 font-medium hover:bg-green-500 hover:text-white rounded-md outline:none">
+                        Anket Ekle
+                    </button>
+                    <button type="submit"
+                        class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Oluştur
+                    </button>
+                </div>
+            </div>
         </form>
     </x-scrollable-wrapper>
 </div>
