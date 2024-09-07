9b5413949eef6d2e0c23c9fe43d1fabf71bd3d1b
yehuuu6 <therenaydin@gmail.com>
2024-09-07 13:57:23 +0300

    Add delete functions

--- app/Livewire/Components/Post/Comment.php
+++ app/Livewire/Components/Post/Comment.php
@@ -3,11 +3,34 @@
 namespace App\Livewire\Components\Post;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Gate;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Comment extends Component
 {
 
+    use LivewireAlert;
+
     public $comment;
+
+    public $postAuthor;
+
+    public function deleteComment()
+    {
+
+        $response = Gate::inspect('delete', $this->comment);
+
+        if (!$response->allowed()) {
+            $this->alert('error', 'Bu yorumu silme izniniz yok.');
+            return;
+        }
+
+        $this->comment->delete();
+
+        $this->alert('success', 'Yorum silindi.');
+
+        $this->dispatch('comment-deleted');
+    }
 
     public function render()
     {
--- app/Livewire/Components/Post/Details.php
+++ app/Livewire/Components/Post/Details.php
@@ -5,9 +5,13 @@ namespace App\Livewire\Components\Post;
 use App\Models\Post;
 use Livewire\Component;
 use Livewire\Attributes\On;
+use Illuminate\Support\Facades\Gate;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Details extends Component
 {
+
+    use LivewireAlert;
 
     public Post $post;
 
@@ -16,11 +20,27 @@ class Details extends Component
         $this->post = $post->loadCount('comments');
     }
 
+    public function deletePost()
+    {
+
+        $response = Gate::inspect('delete', $this->post);
+
+        if (!$response->allowed()) {
+            $this->alert('error', 'Bu konuyu silme izniniz yok.');
+            return;
+        }
+
+        $this->post->delete();
+
+        $this->flash('success', 'Konu silindi.', redirect: route('user.show', $this->post->user));
+    }
+
     #[On('comment-created')]
     #[On('comment-deleted')]
     public function refreshPage()
     {
         $this->post->refresh();
+        $this->post = $this->post->loadCount('comments'); // Load again.
     }
 
     public function render()
