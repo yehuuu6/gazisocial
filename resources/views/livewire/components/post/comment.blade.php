5d725081516f3505e15dc8910eb6a2501de7658f
yehuuu6 <therenaydin@gmail.com>
2024-09-07 13:57:54 +0300

    Display OP

--- resources/views/livewire/components/post/comment.blade.php
+++ resources/views/livewire/components/post/comment.blade.php
@@ -1,20 +1,24 @@
 <li class="flex gap-3 px-5 py-1">
-    <img src="{{ asset($comment->user->avatar) }}" alt="avatar" class="w-8 h-8 rounded-full">
+    <img src="{{ asset($comment->user->avatar) }}" alt="avatar" class="size-8 object-cover rounded-full">
     <div class="w-full flex flex-col gap-2 justify-center">
         <div class="flex justify-between items-center">
             <div class="flex gap-1 items-center">
                 <x-link href="/u/{{ $comment->user->username }}" class="font-medium">
                     {{ $comment->user->name }}
                 </x-link>
-                <p class="text-sm text-gray-500">{{ '@' . $comment->user->username }}</p>
-                <span class="text-gray-500 text-xs">&#x2022;</span>
+                @if ($postAuthor == $comment->user->id)
+                    <span class="ml-1 py-1 px-2 bg-primary text-white font-medium rounded-full capitalize text-xs">
+                        Konu Sahibi
+                    </span>
+                @else
+                    <p class="text-sm text-gray-500">{{ '@' . $comment->user->username }}</p>
+                @endif
+                <span class="inline-block text-gray-500 text-xs">•</span>
                 <p class="text-sm text-gray-500">{{ $comment->created_at->shortAbsoluteDiffForHumans() }}</p>
             </div>
             @auth
                 @can('delete', $comment)
-                    <button
-                        wire:click="$dispatch('openModal', { component: 'modals.delete-comment-modal', arguments: { commentId: {{ $comment->id }} }})"
-                        class="text-sm opacity-60 hover:opacity-100" title="Sil">
+                    <button wire:click="deleteComment" class="text-sm opacity-60 hover:opacity-100" title="Sil">
                         <x-icons.trash color="#ff6969" size="14" />
                     </button>
                 @endcan
