3190f3ad7ba24edcc0fce4ff9d24a9c07b0bf3d1
yehuuu6 <therenaydin@gmail.com>
2024-09-10 00:35:22 +0300

    Refactor Livewire component to update tag colors

--- app/Livewire/Components/Post/Details.php
+++ app/Livewire/Components/Post/Details.php
@@ -26,11 +26,11 @@ class Details extends Component
     {
         $colorVariants = [
             'blue' => 'bg-blue-500',
-            'red' => 'bg-red-700',
-            'green' => 'bg-green-700',
+            'red' => 'bg-red-500',
+            'green' => 'bg-green-500',
             'yellow' => 'bg-yellow-500',
             'indigo' => 'bg-indigo-500',
-            'purple' => 'bg-purple-700',
+            'purple' => 'bg-purple-500',
         ];
 
         shuffle($colorVariants);
