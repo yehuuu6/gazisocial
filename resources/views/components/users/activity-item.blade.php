80ae727508da0a9f194be3273681066aedb51a3d
yehuuu6 <therenaydin@gmail.com>
2024-09-09 22:00:00 +0300

    Refactor user activity item component to use object-cover for avatar image

--- resources/views/components/users/activity-item.blade.php
+++ resources/views/components/users/activity-item.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col user-activity p-4 gap-2">
     <div class="flex justify-between items-center">
         <div class="flex gap-2 items-center">
-            <img class="size-8 rounded-full" src="{{ $activity->user->avatar }}" alt="avatar">
+            <img class="size-8 rounded-full object-cover" src="{{ $activity->user->avatar }}" alt="avatar">
             <x-link href="/u/{{ $activity->user->username }}" class="font-medium">{{ $activity->user->name }}</x-link>
         </div>
         <span class="text-gray-500">{{ $activity->created_at->locale('tr')->shortAbsoluteDiffForHumans() }}</span>
