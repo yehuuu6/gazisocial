0437bc8f02c9d34d7b47b31df4e6a4e9e1eb8356
yehuuu6 <therenaydin@gmail.com>
2024-09-09 16:23:09 +0300

    Add ListFaculties component

---
+++ app/Livewire/Pages/Faculty/ListFaculties.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Faculty;
+
+use Livewire\Component;
+
+class ListFaculties extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.faculty.list-faculties');
+    }
+}
