fa7383e752d3c07e480699f044821e0e7c9b92d6
yehuuu6 <therenaydin@gmail.com>
2024-09-09 16:22:08 +0300

    Refactor search functionality in navbar component

--- app/Livewire/Components/SearchBar.php
+++ app/Livewire/Components/SearchBar.php
@@ -2,28 +2,75 @@
 
 namespace App\Livewire\Components;
 
-use Livewire\Component;
 use App\Models\Post;
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Facades\Route;
 
 class SearchBar extends Component
 {
     public $search = '';
 
+    public $currentRoute;
+    public $targetUrl;
+    public $placeholder;
+
+    public function mount()
+    {
+        $this->currentRoute = Route::currentRouteName();
+        $this->setPlaceholderAndTargetUrl();
+    }
+
     public function render()
     {
-        $posts = [];
 
-        if (strlen($this->search) >= 2) {
-            $posts = Post::with(['user'])
-                ->where('title', 'like', '%' . $this->search . '%' )
-                ->orWhere('content', 'like', '%' . $this->search . '%' )
-                ->latest()
-                ->limit(7)
-                ->get();
-        }
+        $results = $this->getSearchResults();
 
         return view('livewire.components.search-bar', [
-            'posts' => $posts,
+            'results' => $results,
         ]);
+    }
+
+    private function setPlaceholderAndTargetUrl()
+    {
+        if ($this->currentRoute === 'user.show' || $this->currentRoute === 'user.search') {
+            $this->placeholder = 'Bir kullanıcı ara...';
+            $this->targetUrl = '/u/search/';
+        } else {
+            $this->placeholder = 'Bir konu ara...';
+            $this->targetUrl = '/posts/search/';
+        }
+    }
+
+    private function getSearchResults()
+    {
+        if (strlen($this->search) < 2) {
+            return [];
+        }
+
+        if ($this->currentRoute === 'user.show' || $this->currentRoute === 'user.search') {
+            return $this->getUserSearchResults();
+        } else {
+            return $this->getPostSearchResults();
+        }
+    }
+
+    private function getUserSearchResults()
+    {
+        return User::where('name', 'like', '%' . $this->search . '%')
+            ->orWhere('username', 'like', '%' . $this->search . '%')
+            ->latest()
+            ->limit(5)
+            ->get();
+    }
+
+    private function getPostSearchResults()
+    {
+        return Post::with(['user'])
+            ->where('title', 'like', '%' . $this->search . '%')
+            ->orWhere('content', 'like', '%' . $this->search . '%')
+            ->latest()
+            ->limit(4)
+            ->get();
     }
 }
