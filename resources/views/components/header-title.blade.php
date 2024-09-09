356bab9087b32f7797afe0ac1d1f735848726a1a
yehuuu6 <therenaydin@gmail.com>
2024-09-09 16:27:10 +0300

    Refactor header-title.blade.php to add search bar component

--- resources/views/components/header-title.blade.php
+++ resources/views/components/header-title.blade.php
@@ -2,4 +2,5 @@
     <div class="flex gap-2 items-center justify-start">
         <h3 class="text-lg md:text-xl font-bold py-2 md:py-4">{{ $slot }}</h3>
     </div>
+    <livewire:components.search-bar />
 </div>
