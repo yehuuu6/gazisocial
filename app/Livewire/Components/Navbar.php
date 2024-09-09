0293fa14975c2e9fef8f5e557ad6a5f15c0a0e50
yehuuu6 <therenaydin@gmail.com>
2024-09-09 16:21:49 +0300

    Add search functionality to navbar

--- app/Livewire/Components/Navbar.php
+++ app/Livewire/Components/Navbar.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Navbar extends Component
 {
+
+    public function goToSearch($query)
+    {
+        return $this->redirect(route('post.search', $query), navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.components.navbar');
