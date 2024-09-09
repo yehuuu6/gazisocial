42ad8968f6124f4139c1663b0315c64397ee9dd5
yehuuu6 <therenaydin@gmail.com>
2024-09-09 18:05:18 +0300

    Refactor user-post component to display post creation time in Turkish locale

--- resources/views/livewire/components/user/user-post.blade.php
+++ resources/views/livewire/components/user/user-post.blade.php
@@ -2,7 +2,7 @@
     <div class="flex justify-between items-center">
         <x-link href="/posts/{{ Str::slug($post->title) }}"
             class="text-lg font-medium text-blue-500">{{ $post->title }}</x-link>
-        <span class="text-gray-500">{{ $post->created_at->diffForHumans() }}</span>
+        <span class="text-gray-500">{{ $post->created_at->locale('tr')->diffForHumans() }}</span>
     </div>
     <p class="mt-2 text-gray-500 break-all">{{ Str::limit($post->content, 150, '...') }}</p>
 </div>
