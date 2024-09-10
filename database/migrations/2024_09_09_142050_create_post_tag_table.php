96d152ad59160fe98b3aa7f668f7b2323e45b3a3
yehuuu6 <therenaydin@gmail.com>
2024-09-10 12:23:17 +0300

    Refactor post_tag table migration to cascade delete on foreign key constraints

--- database/migrations/2024_09_09_142050_create_post_tag_table.php
+++ database/migrations/2024_09_09_142050_create_post_tag_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Tag::class)->constrained();
-            $table->foreignIdFor(Post::class)->constrained();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
