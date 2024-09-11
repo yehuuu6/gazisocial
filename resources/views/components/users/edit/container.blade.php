dba66ac8f39431ca95e998523759fba2e417da0f
yehuuu6 <therenaydin@gmail.com>
2024-09-11 22:32:48 +0300

    Add container and title components for user edit page

---
+++ resources/views/components/users/edit/container.blade.php
@@ -0,0 +1,3 @@
+<div class="border border-gray-200 rounded-lg">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/users/edit/title.blade.php
@@ -0,0 +1,5 @@
+@props(['title', 'description'])
+<div class="p-4">
+    <h3 class="font-medium text-xl text-gray-800">{{ $title }}</h3>
+    <span class="text-gray-500 font-normal">{{ $description }}</span>
+</div>
