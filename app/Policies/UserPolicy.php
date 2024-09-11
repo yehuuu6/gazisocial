de7ebdf9966ec0242373489938b9ee46c8e645b9
yehuuu6 <therenaydin@gmail.com>
2024-09-11 22:32:27 +0300

    Add UserPolicy for viewing, updating, and deleting user models

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+
+    public function view(User $user, User $model): Response
+    {
+        return $user->id === $model->id
+            ? Response::allow()
+            : Response::deny('Bu sayfayı görüntüleme izniniz yok.');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+}
