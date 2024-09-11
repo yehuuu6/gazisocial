a6939b7f723d92b41beec12efedb7c3f41144669
yehuuu6 <therenaydin@gmail.com>
2024-09-11 22:33:32 +0300

    Update styles

--- resources/views/livewire/modals/create-poll-modal.blade.php
+++ resources/views/livewire/modals/create-poll-modal.blade.php
@@ -1,19 +1,18 @@
-<form wire:submit="createPoll" class="rounded-lg shadow-md">
+<form wire:submit="createPoll" class="rounded-lg">
     @csrf
     <h3 class="text-xl py-4 px-6 text-gray-700 font-medium">Anket oluştur</h3>
     <x-seperator />
-    <div class="p-6 space-y-2">
+    <div class="px-6 py-3 space-y-2">
         <label for="question" class="block text-sm font-medium text-gray-700">Soru</label>
         <input wire:model="question" type="text" id="question" name="question"
             placeholder="Anketiniz için bir soru girin"
-            class="block w-full bg-gray-100 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            class="block w-full bg-gray-50 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             autocomplete="off" />
     </div>
-    <x-seperator />
-    <div class="p-6 space-y-2">
+    <div class="px-6 py-3 space-y-2">
         <div class="flex items-end gap-1">
             <h3 class="block text-sm font-medium text-gray-700">Yanıtlar</h3>
-            <button wire:click.prevent="addOption" type="button" class="text-sm text-blue-400">
+            <button wire:click.prevent="addOption" type="button" class="text-sm text-blue-400 hover:underline">
                 Ekle
             </button>
         </div>
@@ -22,7 +21,7 @@
                 <div class="flex items-center gap-2">
                     <input wire:model="optionInputs.{{ $loop->index }}.option" type="text"
                         id="option-{{ $loop->index }}" name="option-{{ $loop->index }}"
-                        class="block w-full bg-gray-100 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        class="block w-full bg-gray-50 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         placeholder="Yanıt {{ $loop->index + 1 }}" autocomplete="off" />
                     <button wire:click.prevent="removeOption({{ $loop->index }})" type="button" class="p-2">
                         <x-icons.trash color='#ff6969' size='14' />
@@ -34,11 +33,11 @@
     <x-seperator />
     <div class="bg-gray-50 p-4 gap-2 flex items-center justify-end">
         <button wire:click="$dispatch('closeModal')" type="button"
-            class="px-4 py-2 font-medium outline-none text-sm text-red-500 rounded-md hover:bg-red-100">
+            class="px-4 py-2 outline-none font-medium text-red-500 rounded hover:bg-red-100">
             Vazgeç
         </button>
         <button type="submit"
-            class="px-4 py-2 font-medium outline-none text-sm rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+            class="px-6 py-2 bg-blue-500 text-white outline-none font-medium rounded hover:bg-blue-600">
             Ekle
         </button>
     </div>
--- resources/views/livewire/pages/posts/create-post.blade.php
+++ resources/views/livewire/pages/posts/create-post.blade.php
@@ -3,21 +3,21 @@
         Konu Oluştur
     </x-header-title>
     <x-seperator />
-    <x-scrollable-wrapper class="h-full">
-        <form wire:submit="createPost" enctype="multipart/form-data">
-            <div class="p-6 space-y-4">
-                <div class="space-y-2">
-                    <label for="title" class="block text-sm font-medium text-gray-700">Başlık</label>
+    <form wire:submit="createPost" enctype="multipart/form-data" class="flex flex-col h-full">
+        <x-scrollable-wrapper class="flex-grow">
+            <div class="flex flex-col gap-5 py-4">
+                <div class="flex flex-col gap-2 px-4">
+                    <label for="title" class="block font-medium text-gray-700">Başlık</label>
                     <input wire:model="title" type="text" id="title" name="title"
-                        class="bg-gray-100 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        class="block w-full bg-gray-50 px-3 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
                 </div>
-                <div class="space-y-2">
-                    <label for="content" class="block text-sm font-medium text-gray-700">İçerik</label>
+                <div class="flex flex-col gap-2 px-4">
+                    <label for="content" class="block font-medium text-gray-700">İçerik</label>
                     <textarea wire:model="content" id="content" name="content" rows="5"
-                        class="bg-gray-100 resize-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                        class="bg-gray-50 resize-none block w-full px-3 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"></textarea>
                 </div>
-                <div class="space-y-2">
-                    <label for="tags" class="block text-sm font-medium text-gray-700">Etiketler</label>
+                <div class="flex flex-col gap-2 px-4">
+                    <label for="tags" class="block font-medium text-gray-700">Etiketler</label>
                     <div id="tags" class="flex flex-wrap gap-2">
                         @foreach ($tags as $tag)
                             <button wire:click="toggleTag('{{ $tag->id }}')" type="button"
@@ -34,48 +34,51 @@
                         @endforeach
                     </div>
                 </div>
-                <h3 class="text-sm block font-medium text-gray-700">Anketler</h3>
+                <div class="flex flex-col gap-2 px-4">
+                    <h3 class="text-sm block font-medium text-gray-700">Anketler</h3>
 
-                <div class="flex gap-5">
-                    @foreach ($createdPolls as $poll)
-                        <div class="flex flex-col flex-grow gap-1 rounded-md border border-gray-300 bg-gray-100">
-                            <div class="flex items-center justify-between py-2 px-4">
-                                <h3 class="text-md text-gray-700 font-medium">{{ $poll['question'] }}</h3>
-                                <button wire:click.prevent="removePoll({{ $loop->index }})" type="button">
-                                    <x-icons.trash color='#ff6969' size='16' />
-                                </button>
+                    <div class="flex gap-5">
+                        @foreach ($createdPolls as $poll)
+                            <div class="flex flex-col flex-grow gap-1 rounded-md border border-gray-300 bg-gray-100">
+                                <div class="flex items-center justify-between py-2 px-4">
+                                    <h3 class="text-md text-gray-700 font-medium">{{ $poll['question'] }}</h3>
+                                    <button wire:click.prevent="removePoll({{ $loop->index }})" type="button">
+                                        <x-icons.trash color='#ff6969' size='16' />
+                                    </button>
+                                </div>
+                                <x-seperator />
+                                <div class="flex flex-col gap-2 py-1 pb-3">
+                                    @foreach ($poll['options'] as $option)
+                                        <div class="flex items-center px-2 gap-1">
+                                            <span
+                                                class="px-3 py-2 bg-gray-200 rounded-full w-full text-gray-700">{{ $option }}</span>
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
-                            <x-seperator />
-                            <div class="flex flex-col gap-2 py-1 pb-3">
-                                @foreach ($poll['options'] as $option)
-                                    <div class="flex items-center px-2 gap-1">
-                                        <span
-                                            class="px-3 py-2 bg-gray-200 rounded-full w-full text-gray-700">{{ $option }}</span>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
+                    @if (count($createdPolls) === 0)
+                        <h3 class="text-gray-500">Hiç anket eklenmemiş.</h3>
+                    @else
+                        <h3 class="text-orange-400">Oluşturduğunuz anketler taslaktır ve konuyu
+                            yayınlamadığınız
+                            sürece görünmeyecektir.</h3>
+                    @endif
                 </div>
-                @if (count($createdPolls) === 0)
-                    <h3 class="text-gray-500">Hiç anket eklenmemiş.</h3>
-                @else
-                    <h3 class="text-orange-400">Oluşturduğunuz anketler taslaktır ve konuyu
-                        yayınlamadığınız
-                        sürece görünmeyecektir.</h3>
-                @endif
-                <div class="flex gap-2 justify-end">
-                    <button type="button"
-                        wire:click="$dispatch('openModal', { component: 'modals.create-poll-modal' })"
-                        class="py-2 px-5 border border-green-500 outline-none bg-transparent text-green-500 font-medium hover:bg-green-500 hover:text-white rounded outline:none">
-                        Anket Ekle
-                    </button>
-                    <button type="submit" wire:loading.class='animate-pulse'
-                        class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 outline-none">
-                        Yayınla
-                    </button>
-                </div>
+
             </div>
-        </form>
-    </x-scrollable-wrapper>
+        </x-scrollable-wrapper>
+        <x-seperator />
+        <div class="flex justify-end bg-gray-50 p-6 gap-4">
+            <button type="button" wire:click="$dispatch('openModal', { component: 'modals.create-poll-modal' })"
+                class="px-6 py-2 border border-green-500 outline-none bg-transparent text-green-500 font-medium hover:bg-green-500 hover:text-white rounded outline:none">
+                Anket Ekle
+            </button>
+            <button type="submit" wire:loading.class='animate-pulse'
+                class="px-6 py-2 bg-blue-500 text-white font-medium rounded hover:bg-blue-600 outline-none">
+                Yayınla
+            </button>
+        </div>
+    </form>
 </div>
