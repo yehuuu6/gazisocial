53387f0dba2e6988f3bf6d922021552a4ed1ac07
yehuuu6 <therenaydin@gmail.com>
2024-09-12 10:17:56 +0300

    Add user-private-profile component

---
+++ resources/views/livewire/components/user-private-profile.blade.php
@@ -0,0 +1,7 @@
+<div class="flex flex-col h-full">
+    <x-scrollable-wrapper class="flex-grow">
+        <div class="p-5 flex flex-col gap-5 items-center justify-center">
+            <h3 class="text-center text-lg text-gray-600">Bu profil gizli</h3>
+        </div>
+    </x-scrollable-wrapper>
+</div>
