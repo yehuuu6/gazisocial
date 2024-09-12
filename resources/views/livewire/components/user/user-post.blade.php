2ba15965b0d0eda47d89c7fe28499b0cf778ccf8
yehuuu6 <therenaydin@gmail.com>
2024-09-12 15:29:51 +0300

    Refactor post-item.blade.php to use post slug instead of title for the post link

--- resources/views/livewire/components/user/user-post.blade.php
+++ resources/views/livewire/components/user/user-post.blade.php
@@ -1,7 +1,6 @@
 <div class="flex flex-col p-4">
     <div class="flex justify-between items-center">
-        <x-link href="/posts/{{ Str::slug($post->title) }}"
-            class="text-lg font-medium text-blue-500">{{ $post->title }}</x-link>
+        <x-link href="/posts/{{ $post->slug }}" class="text-lg font-medium text-blue-500">{{ $post->title }}</x-link>
         <span class="text-gray-500">{{ $post->created_at->locale('tr')->diffForHumans() }}</span>
     </div>
     <p class="mt-2 text-gray-500 break-all line-clamp-1">{{ $post->content }}</p>
