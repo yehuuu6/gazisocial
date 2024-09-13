30434115e1746927ad05c32e540397a5d37ef6f1
yehuuu6 <therenaydin@gmail.com>
2024-09-13 18:36:21 +0300

    Refactor font in auth layout to use Inter font family

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -11,7 +11,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-white"">
+<body class="bg-white font-inter">
     {{ $slot }}
 </body>
 @livewire('wire-elements-modal')
