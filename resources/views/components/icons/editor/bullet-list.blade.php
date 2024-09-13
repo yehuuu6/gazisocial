cba3595312a0f113a22abaa7f665b3dad6fc1e7e
yehuuu6 <therenaydin@gmail.com>
2024-09-13 18:35:41 +0300

    Add new editor icons

---
+++ resources/views/components/icons/editor/bullet-list.blade.php
@@ -0,0 +1,12 @@
+@props(['size' => '6', 'color' => 'currentColor'])
+
+<svg xmlns="http://www.w3.org/2000/svg" width="{{ $size }}" height="{{ $size }}" viewBox="0 0 24 24"
+    fill="none" stroke="{{ $color }}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-list">
+    <line x1="8" x2="21" y1="6" y2="6" />
+    <line x1="8" x2="21" y1="12" y2="12" />
+    <line x1="8" x2="21" y1="18" y2="18" />
+    <line x1="3" x2="3.01" y1="6" y2="6" />
+    <line x1="3" x2="3.01" y1="12" y2="12" />
+    <line x1="3" x2="3.01" y1="18" y2="18" />
+</svg>
---
+++ resources/views/components/icons/editor/heading-1.blade.php
@@ -0,0 +1,9 @@
+@props(['size' => '6', 'color' => 'currentColor'])
+<svg xmlns="http://www.w3.org/2000/svg" width="{{ $size }}" height="{{ $size }}" viewBox="0 0 24 24"
+    fill="none" stroke="{{ $color }}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-heading-1">
+    <path d="M4 12h8" />
+    <path d="M4 18V6" />
+    <path d="M12 18V6" />
+    <path d="m17 12 3-2v8" />
+</svg>
