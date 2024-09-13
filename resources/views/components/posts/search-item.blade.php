e45a94571ed48c5487fea890c0dc82626402df74
yehuuu6 <therenaydin@gmail.com>
2024-09-13 18:36:03 +0300

    Refactor post search-item component to use dynamic show route

--- resources/views/components/posts/search-item.blade.php
+++ resources/views/components/posts/search-item.blade.php
@@ -1,9 +1,8 @@
 @props(['post'])
 
 <div class="flex flex-col">
-    <x-link href="/posts/{{ $post->slug }}"
-        class="flex gap-2 items-center p-5 hover:bg-gray-100 hover:no-underline"><img class="h-8 w-8 rounded-full"
-            src="{{ $post->user->avatar }}" alt="avatar">
+    <x-link href="{{ $post->showRoute() }}" class="flex gap-2 items-center p-5 hover:bg-gray-100 hover:no-underline"><img
+            class="h-8 w-8 rounded-full" src="{{ $post->user->avatar }}" alt="avatar">
         <h3 class="text-sm font-medium text-blue-800">{{ $post->title }}</h3>
     </x-link>
 </div>
