f825171f93f3733f7f492c2f494519f973cea090
yehuuu6 <therenaydin@gmail.com>
2024-09-14 23:32:22 +0300

    Refactor PostItem component to add random color selection for tags

--- app/Livewire/Components/Post/PostItem.php
+++ app/Livewire/Components/Post/PostItem.php
@@ -10,6 +10,32 @@ class PostItem extends Component
 
     public Post $post;
 
+    protected $selectedColors = [];
+
+    public function getRandomColorForTag()
+    {
+        $colorVariants = [
+            'blue' => 'bg-blue-500',
+            'red' => 'bg-red-500',
+            'green' => 'bg-green-500',
+            'yellow' => 'bg-yellow-500',
+            'indigo' => 'bg-indigo-500',
+            'purple' => 'bg-purple-500',
+        ];
+
+        shuffle($colorVariants);
+
+        foreach ($colorVariants as $color) {
+            if (!in_array($color, $this->selectedColors)) {
+                $this->selectedColors[] = $color;
+                return $color;
+            }
+        }
+
+        // All colors are already selected, handle as needed
+        return "bg-gray-700";
+    }
+
     public function render()
     {
         return view('livewire.components.post.post-item');
