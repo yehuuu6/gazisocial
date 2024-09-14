cc653c3913377859b441bf7354cdd4a184275e37
yehuuu6 <therenaydin@gmail.com>
2024-09-14 23:32:41 +0300

    Update heading levels in editor configuration

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -14,7 +14,7 @@ window.setupEditor = function (content) {
                 extensions: [
                     StarterKit.configure({
                         heading: {
-                            levels: [2, 3, 4],
+                            levels: [3, 4, 5],
                         },
                     }),
                     Link,
