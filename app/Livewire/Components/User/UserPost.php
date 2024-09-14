2c921038cf3fabc1ebae0e0383b9c452ecd7255f
yehuuu6 <therenaydin@gmail.com>
2024-09-14 23:33:43 +0300

    Refactor UserPost component to add delete functionality and display success/error alerts

--- app/Livewire/Components/User/UserPost.php
+++ app/Livewire/Components/User/UserPost.php
@@ -4,11 +4,29 @@ namespace App\Livewire\Components\User;
 
 use App\Models\Post;
 use Livewire\Component;
+use Illuminate\Support\Facades\Gate;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class UserPost extends Component
 {
 
+    use LivewireAlert;
+
     public Post $post;
+
+    public function deletePost()
+    {
+        $response = Gate::inspect('delete', $this->post);
+
+        if ($response->allowed()) {
+            $this->post->delete();
+            $this->alert('success', 'Gönderi başarıyla silindi.');
+            $this->dispatch('userPostDeleted');
+            $this->dispatch('showUserPosts');
+        } else {
+            $this->alert('error', 'Bu işlemi yapmaya yetkiniz yok.');
+        }
+    }
 
     public function render()
     {
