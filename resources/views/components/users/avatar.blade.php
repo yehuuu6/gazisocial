250a02442238dc88f87179a14b89e7ee92edf8a6
yehuuu6 <therenaydin@gmail.com>
2024-09-18 14:55:24 +0300

    Create avatar icon

---
+++ resources/views/components/users/avatar.blade.php
@@ -0,0 +1,17 @@
+@props(['user', 'size' => 24, 'iconSize' => 30])
+@php
+    $sizeClass = 'size-' . $size;
+@endphp
+<div class="relative flex items-center group justify-center rounded-full overflow-hidden">
+    @auth
+        @if (Auth::user()->id === $user->id)
+            <div title="Profil resmini değiştir" @click="$dispatch('openModal', { component: 'modals.update-avatar' })"
+                class="absolute size-full hidden group-hover:grid place-items-center bg-black bg-opacity-50 cursor-pointer">
+                <div id="update-avatar-item">
+                    <x-icons.image size='{{ $iconSize }}' color='#f2f2f2' />
+                </div>
+            </div>
+        @endif
+    @endauth
+    <img src="{{ $user->avatar }}" alt="profil resmi" class="object-cover {{ $sizeClass }}">
+</div>
