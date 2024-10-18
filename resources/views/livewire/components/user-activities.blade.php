b09ceadadcd30822d31bcdbff3dbd9f90b6f1399
yehuuu6 <therenaydin@gmail.com>
2024-10-18 22:46:26 +0300

    Delete unused icons and activity model

--- resources/views/livewire/components/user-activities.blade.php
+++
@@ -1,5 +0,0 @@
-<x-scrollable-wrapper class="h-1/2 hidden sm:inline-block" wire:poll.30s>
-    @foreach ($activities as $activity)
-        <x-users.activity-item :$activity />
-    @endforeach
-</x-scrollable-wrapper>
