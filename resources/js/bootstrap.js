53c0c0999df8e97d3e318181004aa9baf3cbab11
yehuuu6 <therenaydin@gmail.com>
2024-10-24 13:52:47 +0300

    Add Alpine JS copy to clipboard plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,11 +3,13 @@ import collapse from "@alpinejs/collapse";
 import anchor from "@alpinejs/anchor";
 import focus from "@alpinejs/focus";
 import AutoAnimate from "@marcreichel/alpine-auto-animate";
+import Clipboard from "@ryangjchandler/alpine-clipboard";
 
 Alpine.plugin(collapse);
 Alpine.plugin(anchor);
 Alpine.plugin(focus);
 Alpine.plugin(AutoAnimate);
+Alpine.plugin(Clipboard);
 
 window.axios = axios;
 
