54215f97626e062f576546db78772a274d338f39
Eren Aydın <therenaydin@gmail.com>
2024-11-24 11:17:15 +0300

    Add ExampleBugReportModal component and corresponding Blade view for bug reporting

---
+++ app/Livewire/Modals/ExampleBugReportModal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Modals;
+
+use Livewire\Component;
+
+class ExampleBugReportModal extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.modals.example-bug-report-modal');
+    }
+}
