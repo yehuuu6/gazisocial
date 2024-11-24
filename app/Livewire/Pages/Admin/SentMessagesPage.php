643fb0da6dd931aee68c73e62740e972656c4bab
Eren Aydın <49097712+yehuuu6@users.noreply.github.com>
2024-11-24 22:21:59 +0300

    Implement contact message functionality with validation, pagination, and UI enhancements

--- app/Livewire/Pages/Admin/SentMessagesPage.php
+++ app/Livewire/Pages/Admin/SentMessagesPage.php
@@ -3,11 +3,18 @@
 namespace App\Livewire\Pages\Admin;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\ContactMessage;
 
 class SentMessagesPage extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.pages.admin.sent-messages-page');
+        return view('livewire.pages.admin.sent-messages-page', [
+            'messages' => ContactMessage::latest()->simplePaginate(10)
+        ]);
     }
 }
--- resources/views/livewire/pages/admin/sent-messages-page.blade.php
+++ resources/views/livewire/pages/admin/sent-messages-page.blade.php
@@ -1,3 +1,21 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class="overflow-hidden rounded-xl border border-gray-100 bg-white shadow-md">
+    <h1 class="text-3xl font-bold text-gray-900 mx-6 mt-6">Gönderilen Mesajlar</h1>
+    <div class="flex flex-col gap-2 p-4">
+        @forelse ($messages as $message)
+            <div class="flex justify-between md:items-center rounded-md border border-gray-200 bg-gray-50 p-4 gap-10">
+                <div class="flex flex-col gap-1">
+                    <x-link href="{{ route('show-bug', $message) }}"
+                        class="text-lg font-medium">{{ $message->email }}</x-link>
+                    <p class="text-sm text-gray-600">{{ $message->name }}
+                        tarafından
+                        {{ $message->created_at->locale('tr')->diffForHumans() }} bildirildi.</p>
+                </div>
+            </div>
+        @empty
+            <div class="rounded-md shadow p-4">
+                <p class="text-sm text-gray-600">Şu ana kadar kimse mesaj yazmamış.</p>
+            </div>
+        @endforelse
+    </div>
+    {{ $messages->links('livewire.pagination.simple') }}
 </div>
