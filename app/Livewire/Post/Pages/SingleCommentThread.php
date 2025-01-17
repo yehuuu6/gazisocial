d8218037b5e5b130fa87a7af127a0d4b8d34dcb2
yehuuu6 <49097712+yehuuu6@users.noreply.github.com>
2025-01-17 12:48:31 +0300

    Add new component to render single comment under a post

---
+++ app/Livewire/Post/Pages/SingleCommentThread.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Livewire\Post\Pages;
+
+use App\Models\Post;
+use App\Models\Comment;
+use Livewire\Component;
+use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+
+class SingleCommentThread extends Component
+{
+    use LivewireAlert, WithRateLimiting;
+
+    public Post $post;
+    public int $selectedCommentId;
+
+    public function mount(Post $post, int $selectedCommentId)
+    {
+        $this->post = $post;
+        $this->selectedCommentId = $selectedCommentId;
+    }
+
+    public function redirectAuth(string $route)
+    {
+        // Redirect to the login page with the intended route
+        return redirect()->setIntendedUrl($this->post->showRoute())->route($route);
+    }
+
+    public function deleteComment(Comment $comment)
+    {
+
+        $response = Gate::inspect('delete', $comment);
+
+        if (!$response->allowed()) {
+            $this->alert('error', 'Bu yorumu silme izniniz yok.');
+            return;
+        }
+
+        // Rate limit the delete action
+
+        try {
+            $this->rateLimit(30, decaySeconds: 1200);
+        } catch (TooManyRequestsException $exception) {
+            $this->alert('error', "Çok fazla istek gönderdiniz. Lütfen {$exception->minutesUntilAvailable} dakika sonra tekrar deneyin.");
+            return;
+        }
+
+        $countToDecrease = $comment->getAllRepliesCount() + 1; // Add 1 to include the comment itself
+
+        if ($comment->user_id === Auth::id()) {
+            // Update user's last activity
+            $comment->user->heartbeat();
+        }
+
+        $comment->delete();
+
+        $this->alert('success', 'Yorum başarıyla silindi.');
+
+        $this->dispatch('comment-deleted', decreaseCount: $countToDecrease);
+    }
+
+    #[Computed]
+    public function comments()
+    {
+        $comments = Comment::where('post_id', $this->post->id)
+            ->where('id', $this->selectedCommentId)
+            ->with('user')
+            ->withCount('replies')
+            ->oldest()
+            ->simplePaginate(10);
+
+        return $comments;
+    }
+
+    public function render()
+    {
+        return view('livewire.post.pages.single-comment-thread');
+    }
+}
---
+++ resources/views/livewire/post/pages/single-comment-thread.blade.php
@@ -0,0 +1,42 @@
+<div x-data="{
+    registerModal: false,
+    message: '',
+}" x-on:auth-required.window="registerModal = true; message = $event.detail.msg">
+    <div class="p-6">
+        <div
+            class="flex items-center justify-between gap-4 mx-6 mt-1 mb-6 py-3 px-5 rounded-full border border-gray-200">
+            <span class="text-gray-600 min-w-fit font-normal text-sm">
+                Tek yorum gösteriliyor
+            </span>
+            <hr class="w-full border-t border-gray-200">
+            <x-link href="{{ $this->post->showRoute() }}" class="min-w-fit text-blue-400 font-normal text-xs">
+                Tüm yorumları gör
+            </x-link>
+        </div>
+        <div class="px-2">
+            @if (!$this->comments->isNotEmpty())
+                <div class="flex gap-3 items-center">
+                    <div>
+                        <img src="{{ asset('discussion.png') }}" alt="start convo" class="size-20">
+                    </div>
+                    <div>
+                        <h3 class="font-semibold text-gray-600 text-base mb-3">İlk yorum yapan siz olun</h3>
+                        <p class="text-gray-500 font-normal text-sm">
+                            Bu gönderiye henüz yorum yapılmamış.
+                            <br>
+                            Tartışmayı başlatmak için ilk yorumu siz yapın.
+                        </p>
+                    </div>
+                </div>
+            @else
+                <div class="space-y-2.5">
+                    @foreach ($this->comments as $comment)
+                        <livewire:post.comment-item :$post :$comment :key="'comment-' . $comment->id" lazy />
+                    @endforeach
+                </div>
+            @endif
+        </div>
+    </div>
+    {{ $this->comments->links('livewire.pagination.simple') }}
+    <x-user.register-modal wire:modal="registerModal" />
+</div>
