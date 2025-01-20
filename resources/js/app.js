81279e4f3989d9ce58cccfd6f9a00a84a0cf0105
yehuuu6 <49097712+yehuuu6@users.noreply.github.com>
2025-01-20 15:22:09 +0300

    Load alpine and livewire manually with plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import "./bootstrap";
+
+import {
+    Livewire,
+    Alpine,
+} from "../../vendor/livewire/livewire/dist/livewire.esm";
+import collapse from "@alpinejs/collapse";
+import anchor from "@alpinejs/anchor";
+import focus from "@alpinejs/focus";
+import AutoAnimate from "@marcreichel/alpine-auto-animate";
+import Clipboard from "@ryangjchandler/alpine-clipboard";
+
+Alpine.plugin(collapse);
+Alpine.plugin(anchor);
+Alpine.plugin(focus);
+Alpine.plugin(AutoAnimate);
+Alpine.plugin(Clipboard);
+
+Livewire.start();
