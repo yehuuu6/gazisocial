fd39f27034074e36a6177c5f9b5e9f19181bb109
yehuuu6 <therenaydin@gmail.com>
2025-03-20 21:26:07 +0300

    Add About Us page and update routing; enhance faculty display styles

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,88 +1,75 @@
 <div class="mt-auto">
     <footer class="py-12 px-[3%] xl:px-[6%] 2xl:px-[12%] bg-blue-950">
-        <div class="flex flex-col md:flex-row md:items-center justify-between gap-6">
+        <div class="flex flex-col md:flex-row md:items-center justify-between gap-4 md:gap-6">
             <div class="mb-4 md:mb-0">
                 <div class="w-36 md:w-48 mb-3.5">
                     <img src="{{ asset('\logos/GS_LOGO_BLUE_WHITE.png') }}" alt="logo" class="size-full object-cover">
                 </div>
-                <p class="text-gray-100 text-lg md:text-xl italic font-satisfy">
+                <p class="text-gray-100 text-lg md:text-xl italic font-medium font-satisfy">
                     Gazili olmak ayrıcalıktır
                 </p>
             </div>
-            <div class="flex gap-2 flex-col text-gray-200">
-                <h6 class="text-white font-semibold mb-1 text-lg">Gazi Social</h6>
-                <div class="flex flex-col gap-2">
-                    <x-link href="{{ route('home') }}" class="font-normal text-sm self-start">
-                        Ana Sayfa
-                    </x-link>
-                    <x-link href="{{ route('about') }}" class="font-normal text-sm self-start">
-                        Hakkımızda
-                    </x-link>
-                    @guest
-                        <x-link href="{{ route('register') }}" class="font-normal text-sm self-start">
-                            Kayıt Ol
+            <div class="flex flex-col md:flex-row md:items-center justify-end md:justify-start gap-6 md:gap-24">
+                <div class="flex gap-2 flex-col text-gray-200">
+                    <h6 class="text-white font-semibold mb-1 text-lg">Gazi Social</h6>
+                    <div class="flex flex-col gap-2">
+                        <x-link href="{{ route('home') }}" class="font-medium text-sm self-start md:self-end">
+                            Ana Sayfa
                         </x-link>
-                    @endguest
-                    @auth
-                        <x-link href="{{ route('users.edit', Auth::user()->username) }}"
-                            class="font-normal text-sm self-start">
-                            Hesabım
+                        <x-link href="{{ route('about') }}" class="font-medium text-sm self-start md:self-end">
+                            Hakkımızda
                         </x-link>
-                    @endauth
+                        @guest
+                            <x-link href="{{ route('register') }}" class="font-medium text-sm self-start md:self-end">
+                                Kayıt Ol
+                            </x-link>
+                        @endguest
+                        @auth
+                            <x-link href="{{ route('users.edit', Auth::user()->username) }}"
+                                class="font-medium text-sm self-start md:self-end">
+                                Hesabım
+                            </x-link>
+                        @endauth
+                    </div>
                 </div>
-            </div>
-            <div class="flex gap-2 flex-col text-gray-200">
-                <h6 class="text-white font-semibold mb-1 text-lg">Oyun Merkezi</h6>
-                <div class="flex flex-col gap-2">
-                    <x-link href="{{ route('games.index') }}" class="font-normal text-sm self-start">
-                        Oyunlar
-                    </x-link>
-                    <x-link href="{{ route('games.zk.guide') }}" class="font-normal text-sm self-start">
-                        Zalim Kasaba
-                    </x-link>
-                    <x-link href="games/cb" class="font-normal text-sm self-start">
-                        Çiz Bil
-                    </x-link>
+                <div class="flex gap-2 flex-col text-gray-200">
+                    <h6 class="text-white font-semibold mb-1 text-lg">Oyun Merkezi</h6>
+                    <div class="flex flex-col gap-2">
+                        <x-link href="{{ route('games.index') }}" class="font-medium text-sm self-start md:self-end">
+                            Oyunlar
+                        </x-link>
+                        <x-link href="{{ route('games.zk.guide') }}" class="font-medium text-sm self-start md:self-end">
+                            Zalim Kasaba
+                        </x-link>
+                        <x-link href="games/cb" class="font-medium text-sm self-start md:self-end">
+                            Çiz Bil
+                        </x-link>
+                    </div>
                 </div>
-            </div>
-            <div class="flex gap-2 flex-col text-gray-200">
-                <h6 class="text-white font-semibold mb-1 text-lg">Yardım</h6>
-                <div class="flex flex-col gap-2">
-                    <x-link href="{{ route('faq') }}" class="font-normal text-sm self-start">
-                        SSS
-                    </x-link>
-                    <x-link href="{{ route('privacy') }}" class="font-normal text-sm self-start">
-                        Gizlilik Politikası
-                    </x-link>
-                    <x-link href="{{ route('terms') }}" class="font-normal text-sm self-start">
-                        Kullanım Koşulları
-                    </x-link>
-                </div>
-            </div>
-            <div class="flex gap-2 flex-col text-gray-200">
-                <h6 class="text-white font-semibold mb-1 text-lg">İletişim</h6>
-                <div class="flex flex-col gap-2">
-                    <x-link href="#" class="font-normal text-sm self-start">
-                        Mesaj Gönder
-                    </x-link>
-                    <x-link href="#" class="font-normal text-sm self-start">
-                        Hata Bildir
-                    </x-link>
-                    <x-link href="#" class="font-normal text-sm self-start">
-                        Bilinen Hatalar
-                    </x-link>
+                <div class="flex gap-2 flex-col text-gray-200">
+                    <h6 class="text-white font-semibold mb-1 text-lg self-start md:self-end">Yardım</h6>
+                    <div class="flex flex-col gap-2">
+                        <x-link href="{{ route('faq') }}" class="font-medium text-sm self-start md:self-end">
+                            SSS
+                        </x-link>
+                        <x-link href="{{ route('privacy') }}" class="font-medium text-sm self-start md:self-end">
+                            Gizlilik Politikası
+                        </x-link>
+                        <x-link href="{{ route('terms') }}" class="font-medium text-sm self-start md:self-end">
+                            Kullanım Koşulları
+                        </x-link>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="my-8 opacity-30">
             <x-seperator />
         </div>
-        <div class="w-full text-gray-300 mb-1.5 text-sm md:text-center">
+        <div class="w-full font-medium text-gray-300 mb-1.5 text-sm md:text-center">
             <span>Made with 🧠 by </span>
-            <a href="https://github.com/yehuuu6" target="_blank"
-                class="text-blue-200 font-normal hover:underline">@yehuuu6</a>
+            <a href="https://github.com/yehuuu6" target="_blank" class="text-blue-200 hover:underline">@yehuuu6</a>
         </div>
-        <div class="w-full text-gray-300 md:text-center text-sm">
+        <div class="w-full font-medium text-gray-300 md:text-center text-sm">
             <span>Copyright © Gazi Social <span x-data="{ date: '2025' }" x-init="date = new Date().getFullYear()" x-text="date"></span>
                 All rights
                 reserved.</span>
---
+++ resources/views/livewire/docs/about-us.blade.php
@@ -0,0 +1,22 @@
+<div class="rounded-xl border border-gray-100 bg-white shadow-md p-8">
+    <h1 class="text-3xl font-bold text-gray-900 mb-6">Hakkımızda (About Us)</h1>
+    <p class="text-gray-700 w-2/3">
+        Gazi Social, Haziran 2024'te <a target="_blank" class="text-blue-400 hover:underline"
+            href="https://github.com/yehuuu6">Eren Aydın</a> tarafından geliştirilmeye
+        başlanan bağımsız bir projedir. Nisan 2025'te yayın hayatına başlamıştır. Proje, başlangıç
+        zamanlarında open-source (açık kaynak) olarak geliştirilmiş, ancak daha sonra kapalı kaynak olarak devam
+        etmiştir. Projenin geliştirilmesine yayınlandıktan sonra bir müddet daha devam edilecektir. Geliştirilirken ana
+        hedef, Laravel ve Livewire frameworkünü daha iyi anlamak ve pekiştirmek olmuştur.
+    </p>
+    <p class="mt-4 text-gray-800 w-2/3 font-semibold">
+        Sitede karşılaştığınız hataları veya önerilerinizi iletişim adresimizden bize bildirebilirsiniz.
+    </p>
+    <div class="mt-5 font-semibold">
+        İletişim: <a class="text-blue-400 hover:underline font-normal" href="mailto:contact@gazisocial.com">
+            contact@gazisocial.com
+        </a>
+    </div>
+    <div class="w-36 md:w-48 mt-5">
+        <img src="{{ asset('\logos/GS_LOGO_DEFAULT.png') }}" alt="logo" class="size-full object-cover">
+    </div>
+</div>
