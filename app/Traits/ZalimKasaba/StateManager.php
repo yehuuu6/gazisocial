4278bdb45727a21ed88cab62a921583f95a10d38
Eren Aydın <49097712+yehuuu6@users.noreply.github.com>
2025-03-28 09:45:04 +0300

    Update StateManager.php

--- app/Traits/ZalimKasaba/StateManager.php
+++ app/Traits/ZalimKasaba/StateManager.php
@@ -47,19 +47,19 @@ trait StateManager
         $this->lobby->update(['state' => $nextState]);
 
         // In seconds
-        $actualTimerValues = [
+        $timerValues = [
             GameState::PREPARATION->value => 15,
-            GameState::DAY->value => $currentDay === 0 ? 20 : 45,
+            GameState::DAY->value => $currentDay === 0 ? 10 : 45,
             GameState::VOTING->value => 30,
             GameState::DEFENSE->value => 20,
             GameState::JUDGMENT->value => 20,
             GameState::LAST_WORDS->value => 10,
             GameState::NIGHT->value => 45,
-            GameState::REVEAL->value => 5,
+            GameState::REVEAL->value => 10,
             GameState::GAME_OVER->value => 30,
         ];
 
-        $timerValues = [
+        $oldTimerValues = [
             GameState::PREPARATION->value => 5,
             GameState::DAY->value => $currentDay === 0 ? 5 : 30,
             GameState::VOTING->value => 15,
