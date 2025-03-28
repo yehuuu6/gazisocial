ec341985e989d0413c28424062225d66ed95c584
yehuuu6 <therenaydin@gmail.com>
2025-03-28 20:43:50 +0300

    Add console command to assign 'gazi-social' role to a specific user

---
+++ app/Console/Commands/GiveRoleToMe.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class GiveRoleToMe extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'op-me';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = User::where('email', 'eren.aydin@gazi.edu.tr')->first();
+        $role = Role::where('slug', 'gazi-social')->first();
+
+        if ($user && $role) {
+            $user->roles()->attach($role);
+            $this->info('Role assigned successfully.');
+        } else {
+            $this->error('User or role not found.');
+        }
+    }
+}
