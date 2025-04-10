01ef0138f661a8b740b9e78d162e5dca5fd5f8dd
Eren Aydın <therenaydin@gmail.com>
2025-04-10 14:34:58 +0300

    Add email notification templates and styles for password reset and email verification

--- app/Notifications/Auth/QueuedResetPassword.php
+++ app/Notifications/Auth/QueuedResetPassword.php
@@ -6,9 +6,26 @@ use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
 class QueuedResetPassword extends ResetPassword implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * Get the reset password notification mail message for the given URL.
+     *
+     * @param  string  $url
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage)
+            ->subject("Şifre Sıfırlama İsteği")
+            ->line("Gazi Social hesabınız için bir şifre sıfırlama isteği aldık. Şifrenizi sıfırlamak için lütfen aşağıdaki butona tıklayın.")
+            ->action("Şifremi Sıfırla", $url)
+            ->line(
+                'Bu bağlantı 60 dakika boyunca geçerlidir.'
+            )
+            ->line("Eğer bu şifre sıfırlama isteğini siz yapmadıysanız, lütfen bu e-postayı dikkate almayınız.");
+    }
 }
